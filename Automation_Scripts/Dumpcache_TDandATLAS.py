c9e8bf654ffc4e4854ceb27cb77c044c5edd1cb7
iherasym <iryna.herasymchuk@refinitiv.com>
2024-07-16 22:36:52 +0300

    Added a bit of printing :)))

--- Automation_Scripts/Dumpcache_TDandATLAS.py
+++ Automation_Scripts/Dumpcache_TDandATLAS.py
@@ -1,6 +1,6 @@
 """
 This script allows the user to create and download the DumpCache file for a desired venue and to get from it the most
-active RIC for every CID and Domain.
+active RICs for every CID and Domain.
 """
 import paramiko
 from paramiko.ssh_exception import AuthenticationException
@@ -13,30 +13,36 @@ import re
 import csv
 import os
 
+
 def main():
-    global output_host
+    type = input("Is it ATLAS or TD: ")
     hostname = input("provide server Ip: ")
     password = input("provide password: ")
     LH_name = input("provide LH names separated by space: ").split()
-    type = input("Atlas or TD: ")
-    local_path = input("provide a location where you want to save DumpCache files to your local machine: ")
-    today = datetime.datetime.now().strftime("%Y%m%d")
-    fo4 = open(local_path + "Sample_RICs.txt", "w")
-    fo4.write(f"Sample RICs for:\n\n")
-    fo4.close()
+    global output_host
     output_host = output_host(hostname, password)
+    local_path = input(f"provide  location on your local machine to save dumpCache files: ") + output_host + "\\"
+    os.makedirs(local_path,exist_ok=True)
+    today = datetime.datetime.now().strftime("%Y%m%d")
+    foSample_RICs = open(local_path + "Sample_RICs.txt", "w")
+    foSample_RICs.write(f"Sample RICs for:\n\n")
+    foSample_RICs.close()
     for LH in LH_name:
-        if type == "Atlas":
+        if type == "ATLAS":
             dumpcache_file = download_dumpcache_atlas(hostname, password, LH, local_path, today, output_host)
-            CID = find_CID_dumpcache(local_path, dumpcache_file)
-            sample_RIC_per_CID(type, local_path, dumpcache_file, CID, LH)
+            CID = find_CID_dumpcache(local_path, dumpcache_file, LH)
+            sample_RIC(type, local_path, dumpcache_file, CID, LH)
         elif type == "TD":
-            dumpcache_file = download_dumpcache(hostname, password, LH, local_path, today ,output_host)
-            CID = find_CID_dumpcache(local_path, dumpcache_file)
-            sample_RIC_per_CID(type, local_path, dumpcache_file, CID, LH)
+            dumpcache_file = download_dumpcache_TD(hostname, password, LH, local_path, today, output_host)
+            CID = find_CID_dumpcache(local_path, dumpcache_file, LH)
+            sample_RIC(type, local_path, dumpcache_file, CID, LH)
+    os.remove(local_path + "temp_dump_file.csv")
+    print(f"Now all files are on your local machine in: {local_path}\nThank you. Bye :)")
 
+
+# this function gets server's hostname
 def output_host(hostname, password):
-    ssh = paramiko.SSHClient()  # create ssh client
+    ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname=hostname, username="root", password=password, port=22)
     stdin, stdout, stderr = ssh.exec_command(f"hostname")
@@ -47,33 +53,29 @@ def output_host(hostname, password):
     return output_host
 
 
-# this function is to download the DumpCache and FidFilter files to your local machine#
-# this function is to download the DumpCache and FidFilter files to your local machine#
-def download_dumpcache(hostname, password, LH, local_path, today ,output_host):
+# this function downloads DumpCache files from TD venue to user's local machine
+def download_dumpcache_TD(hostname, password, LH, local_path, today, output_host):
     try:
         ssh = paramiko.SSHClient()  # create ssh client
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(hostname=hostname, username="root", password=password, port=22)
-        print("I am connected to download DumpCache file")
+        print(f"---------------------------------------------------\nPlease wait, dumpcache creation for {LH}:")
         stdin, stdout, stderr = ssh.exec_command(f"cd /data/che/bin ; pwd ; ./Commander -n linehandler -c 'dumpcache {LH}'")
         time.sleep(10)
         outp = stdout.readlines()
         dumpcache_file = LH + "_" + today + ".csv"
-        print(dumpcache_file)
         stdin, stdout, stderr = ssh.exec_command(f"find / -name " + dumpcache_file)
         time.sleep(2)
         config_ph = stdout.read()
         config_ph = config_ph.decode(encoding="utf-8")
         config_ph = ''.join(c for c in config_ph if c.isprintable())
         sftp_client = ssh.open_sftp()
-        print("DumpCache file is downloading")
         sftp_client.get(config_ph, local_path + output_host + "_" + dumpcache_file)
+        print("    (1/3) - " + dumpcache_file + " is downloaded")
         dumpcache_file = output_host + "_" + dumpcache_file
-        print("Dumpcache file - download completed")
         sftp_client.close()
         ssh.close
         return dumpcache_file
-
 
     except socket.gaierror:
         print("Connection Error make sure server ip provided is correct and you are connected to the LSEG VPN")
@@ -97,23 +99,23 @@ def download_dumpcache(hostname, password, LH, local_path, today ,output_host):
         print(e)
         quit()
 
+
+# this function downloads DumpCache files from ATLAS venue to user's local machine
 def download_dumpcache_atlas(hostname, password, LH, local_path, today, output_host):
     try:
-        ssh = paramiko.SSHClient()  # create ssh client
+        ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(hostname=hostname, username="root", password=password, port=22)
-        print("I am connected to download DumpCache file")
+        print(f"---------------------------------------------------\nPlease wait, dumpcache creation for {LH}:")
         stdin, stdout, stderr = ssh.exec_command(f"cd .. ; cd tmp ; commander.sh {LH} cache dump > {LH}_{today}.csv")
         time.sleep(10)
         outp = stdout.readlines()
         dumpcache_file = LH + "_" + today + ".csv"
-        print(dumpcache_file)
         sftp_client = ssh.open_sftp()
-        print("DumpCache file is downloading")
         path = "/tmp/" + dumpcache_file
         sftp_client.get(path, local_path + output_host + "_" + dumpcache_file)
+        print("    (1/3) - " + dumpcache_file + " is downloaded")
         dumpcache_file = output_host + "_" + dumpcache_file
-        print("Dumpcache file - download completed")
         sftp_client.close()
         ssh.close
         return dumpcache_file
@@ -140,79 +142,89 @@ def download_dumpcache_atlas(hostname, password, LH, local_path, today, output_h
         print(e)
         quit()
 
-def find_CID_dumpcache(local_path, dumpcache_file):
-    file = local_path + dumpcache_file
-    def getColumn(name):
-        return [row[name] for row in data]
 
-    with open(file, newline='') as new_csv_file:
+# this function gets the list of CONTEXT_ID for each LineHandler
+def find_CID_dumpcache(local_path, dumpcache_file, LH):
+    temp = local_path + dumpcache_file
+    CID_all = []
+    with open(temp, newline='') as new_csv_file:
         new_csv_file = csv.DictReader(new_csv_file)
         data = list(new_csv_file)
-    CID_all = []
-
-    for u in getColumn('CONTEXT_ID'):
-        CID_all.append(u)
-
+    def getColumn(name):
+        return [row[name] for row in data]
+    for n in getColumn('CONTEXT_ID'):
+        CID_all.append(n)
     while ("" in CID_all):
         CID_all.remove("")
     CID = list(set((CID_all)))
+    for c in CID:
+        if c == '0':
+            CID.remove(c)
+    print(f"    (2/3) - CONTEXT_ID: {CID}")
     return CID
 
 
-def sample_RIC_per_CID(type, local_path, dumpcache_file, CID, LH):
+# this function gets the most active Sample RICs for each CONTEXT_ID
+def sample_RIC(type, local_path, dumpcache_file, CID, LH):
     data = pd.read_csv(local_path + dumpcache_file)
     if type == "TD":
-        columns_to_drop = ['ITEM_ID', 'LAST_UPDATED', 'LAST_ACTIVITY', 'TIME_CREATED', 'VEHICLEID']
-    elif type == "Atlas":
-        columns_to_drop = ['VEHICLE_ID', 'LAST_UPDATED', 'LAST_ACTIVITY', 'TIME_CREATED', 'ISSUTYPE']
-    data.drop(columns=columns_to_drop, inplace=True, axis=1)
-    data.to_csv(local_path + 'new_dump_file.csv', index=False)
-    new_dump_file = 'new_dump_file.csv'
-    fo = open(local_path + new_dump_file, "r")
+        drop_columns = ['ITEM_ID', 'LAST_UPDATED', 'LAST_ACTIVITY', 'TIME_CREATED', 'VEHICLEID']
+    elif type == "ATLAS":
+        drop_columns = ['VEHICLE_ID', 'LAST_UPDATED', 'LAST_ACTIVITY', 'TIME_CREATED', 'ISSUTYPE']
+    data.drop(columns = drop_columns, inplace=True, axis=1)
+    data.to_csv(local_path + 'temp_dump_file.csv', index=False)
+    temp_dump_file = 'temp_dump_file.csv'
+    fo = open(local_path + temp_dump_file, "r")
     content = fo.readlines()
-    fo4 = open(local_path + "Sample_RICs.txt", "a")
-    fo4.write(f"*** {LH} ***\n")
+    foSample_RICs = open(local_path + "Sample_RICs.txt", "a")
+    foSample_RICs.write(f"*** {LH} ***\n")
     for c in CID:
-        fo1 = open(local_path + "CID_" + c + "_RIC_LIST.csv", "w")
-        fo1.write(list(content)[0])
+        foRIC_LIST = open(local_path + "CID_" + c + "_RIC_LIST.csv", "w")
+        foRIC_LIST.write(list(content)[0])
         patt1 = r"\b" + c + "\\b"
         for line in content:
             if re.findall(patt1, line):
-                fo1.writelines(line)
-        fo1.close()
+                foRIC_LIST.writelines(line)
+        foRIC_LIST.close()
         data = pd.read_csv(local_path + "CID_" + c + "_RIC_LIST.csv")
         CURR_SEQ_NUM = data["CURR_SEQ_NUM"].tolist()
         CURR_SEQ_NUM.sort()
         highest = CURR_SEQ_NUM[-1]
         highest = str(highest)
-        fo2 = open(local_path + "CID_" + c + "_RIC_LIST.csv", "r")
-        content_highest = fo2.readlines()
-        fo3 = open(local_path + c + "_Sample_RIC.csv", "w", newline="")
-        fo3.write(list(content)[0])
+        foHighest = open(local_path + "CID_" + c + "_RIC_LIST.csv", "r")
+        content_highest = foHighest.readlines()
+        foRICs = open(local_path + c + "_Sample_RIC.csv", "w", newline="")
+        foRICs.write(list(content)[0])
         patt2 = r"\b" + highest + "\\b"
         for l in content_highest:
             if re.findall(patt2, l):
-                fo3.writelines(l, )
-        fo2.close()
-        fo3.close()
+                foRICs.writelines(l, )
+        foHighest.close()
+        foRICs.close()
         Sample_RIC = pd.read_csv(f"{local_path}{c}_Sample_RIC.csv")
         PUBLISH_KEY = Sample_RIC["PUBLISH_KEY"].tolist()
         DOMAIN = Sample_RIC["DOMAIN"].tolist()
         for K in PUBLISH_KEY:
             for D in DOMAIN:
-                fo4.write(f"{D} - {c} - {K} \n")
+                foSample_RICs.write(f"{D} - {c} - {K} \n")
                 break
             break
         os.remove(local_path + "CID_" + c + "_RIC_LIST.csv")
         os.remove(local_path + c + "_Sample_RIC.csv")
-        #os.remove(local_path + new_dump_file)
-    fo4.close()
+    foSample_RICs.close()
+    print(f"    (3/3) - Sample RICs are gathered")
 
 
 main()
 
-#provide server Ip: 10.167.157.83
-#provide password: Reuters1
-#provide LH names separated by space: CSA01
-#Atlas or TD: Atlas
-#provide a location where you want to save DumpCache files to your local machine: C:\Users\UC502266\Documents\Functional\CSA\
+#TD
+#10.167.119.144
+#Reuters1
+#OPTIQ04 OPTIQ06 OPTIQ08 OPTIQ09
+#C:\Users\UC502266\Documents\Functional\OPTIQ\Euronext_Cash_SBE335\Python_test\
+
+#ATLAS
+#10.167.157.83
+#Reuters1
+#CSA01
+#C:\Users\UC502266\Documents\Functional\CSA\
